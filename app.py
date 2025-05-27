360d5a3263f9687d12c208da4b23d10bf7abb982
Memeathon <rafilalam009@gmail.com>
2025-05-27 17:39:58 +0800

    Pushed Major Changes

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 import os
-import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/design')
+def design():
+    return render_template('design.html')
+
+@app.route('/build')
+def build():
+    return render_template('build.html')
+
+@app.route('/think')
+def think():
+    return render_template('think.html')
+
+@app.route('/share')
+def share():
+    return render_template('share.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
